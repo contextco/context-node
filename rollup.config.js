728aad68698f8a95cef2451cad1ae97b0c1ff93c
Alex Gamble <agamble.uk@gmail.com>
2023-08-21 19:10:27 +0100

    add cjs + esm version

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,21 @@ import typescript from '@rollup/plugin-typescript';
 import cjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
+import pkg from './package.json';
+
 const config = [
   {
     input: 'src/index.ts',
-    output: {
-      dir: 'build',
-      format: 'es'
-    },
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
     plugins: [typescript({ tsconfig: './tsconfig.json' }), nodeResolve(), cjs(), json()],
   }
 ]
