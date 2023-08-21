94fd9010f7d83d3dd71aa2361f9f40db91bde914
Alex Gamble <agamble.uk@gmail.com>
2023-08-21 18:48:45 +0100

    attempt to fix type resolution by deleting an older declaration file

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import cjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import dts from "rollup-plugin-dts";
 
 const config = [
   {
@@ -12,12 +11,7 @@ const config = [
       format: 'es'
     },
     plugins: [typescript({ tsconfig: './tsconfig.json' }), nodeResolve(), cjs(), json()],
-  },
-  {
-    input: "build/ts/index.d.ts",
-    output: [{ file: "build/index.d.ts", format: "es" }],
-    plugins: [dts()],
-  },
+  }
 ]
 
 export default config;
